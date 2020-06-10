9c7aa176068bd43a1ddee3fdd4447439fc94ada3
WilliamBasquez <webasquez@miners.utep.edu>
2020-06-10 15:53:11 -0600

    Initial Commit

---
+++ binaryReversal.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int spitNewNum(int*, int);
+int numOfSpaces(char[]);
+void arrayFilled(char[], int, int[]);
+
+int main()
+{
+    char string[100];
+    printf("Enter the number you want to find the binary reverse: ");
+    fgets(string, 100, stdin);
+    int spaces = numOfSpaces(string);
+    printf("Using %d spaces\n", spaces);
+    int newArray[spaces];
+    arrayFilled(string, spaces, newArray);
+    printf("returning: %d\n", spitNewNum(newArray, spaces));
+
+    return 0;
+}
+
+int numOfSpaces(char str[]){
+    int binary = atoi(str), items = 0;
+    int power = 1;
+    
+    while(binary > 0){
+        items++;
+        binary /= 2;
+    }
+    
+    while(power < items || power % 4 != 0)
+        power = power * 2;
+    
+    return power;
+}
+
+void arrayFilled(char str[], int length, int *incoming){
+    int f = 0;
+    for(f; f < length; f++)
+        incoming[f] = 0;
+
+    int o = length-1;
+    int num = atoi(str);
+    
+    for(o; o > -1; o--){
+        incoming[o] = num % 2;
+        num /= 2;
+    }
+}
+
+int spitNewNum(int *input, int len){
+    int result = 0, counter = 0, power = 1;
+    
+    while(counter < len){
+        result += *(input+counter) * power;
+        power *= 2;
+        counter++;
+    }
+    
+    return result;
+}
+
